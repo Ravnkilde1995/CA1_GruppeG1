acb4aa0dbf0d9d3798d608a966d84fcd6e60e585
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2023-03-17 17:01:37 +0100

    Create CityInfoDTOtest.java
    Created CityInfoDTOtest.java.
    
    Work In Progress.

---
+++ src/test/java/dtos/CityInfoDTOtest.java
@@ -0,0 +1,9 @@
+package dtos;
+
+public class CityInfoDTOtest {
+public static void main(String[] args) {
+        CityInfoDTO cityInfoDTO = new CityInfoDTO(1, "Aalborg", 9000);
+        System.out.println(cityInfoDTO);
+    }
+
+}
