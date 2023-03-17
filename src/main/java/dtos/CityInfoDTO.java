f703835c2c64530037692c8b5447a0ead257266c
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2023-03-17 10:15:59 +0100

    Update CityInfoDTO.java
    Updated CityInfoDTO.java. Added variables "city" and "zipCode".

--- src/main/java/dtos/CityInfoDTO.java
+++ src/main/java/dtos/CityInfoDTO.java
@@ -4,14 +4,26 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class CityInfoDTO implements Serializable {
-    private final Integer id;
+    private final int id;
+    private final String city;
+    private final int zipCode;
 
-    public CityInfoDTO(Integer id) {
+    public CityInfoDTO(int id, String city, int zipCode) {
         this.id = id;
+        this.city = city;
+        this.zipCode = zipCode;
     }
 
-    public Integer getId() {
+    public int getId() {
         return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getZipCode() {
+        return zipCode;
     }
 
     @Override
@@ -19,17 +31,21 @@ public class CityInfoDTO implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CityInfoDTO entity = (CityInfoDTO) o;
-        return Objects.equals(this.id, entity.id);
+        return Objects.equals(this.id, entity.id) &&
+                Objects.equals(this.city, entity.city) &&
+                Objects.equals(this.zipCode, entity.zipCode);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(id, city, zipCode);
     }
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "(" +
-                "id = " + id + ")";
+                "id = " + id + ")" +
+                "city = " + city + ")" +
+                "zipCode = " + zipCode + ")";
     }
 }
