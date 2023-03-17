a3e363d37c059d013a32a2596f9b3d92a92f110a
Mawl026 <71074985+Mawl026@users.noreply.github.com>
2023-03-17 10:17:12 +0100

    Update CityInfo.java
    Updated CityInfo.java. Added the variables "city" and "zipCode".

--- src/main/java/entities/CityInfo.java
+++ src/main/java/entities/CityInfo.java
@@ -7,14 +7,45 @@ public class CityInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idCityInfo", nullable = false)
-    private Integer id;
+    private int id;
 
-    public Integer getId() {
+    @Column(name = "city", nullable = false, length = 60)
+    private String city;
+
+    @Column(name = "zipCode", nullable = false, length = 4)
+    private int zipCode;
+
+    public CityInfo(int id, String city,int zipCode ) {
+        this.id = id;
+        this.city = city;
+        this.zipCode= zipCode;
+    }
+
+    public CityInfo() {
+    }
+
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
     }
 
     //TODO Reverse Engineering! Migrate other columns to the entity
